f46a86be949ef8ae2426c87d1141cb9bee07a251
jongyul-lee <dlwhddbf0614@gmail.com>
2025-04-26 11:08:57 +0900

    폴더구조 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home")
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs"); 
+
+app.use("/", home); // use -> middle ware를 등록해주는 메서드. / 주소로 들어오는 요청들을 home에서 처리하겠다는 뜻
+
+module.exports = app;
+
