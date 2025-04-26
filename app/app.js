7e519e7bc1f7cff82773a104ef7e17e928676de5
jongyul-lee <dlwhddbf0614@gmail.com>
2025-04-26 11:16:41 +0900

    프론트 단 js 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home")
 app.set("views", "./src/views");
 app.set("view engine", "ejs"); 
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home); // use -> middle ware를 등록해주는 메서드. / 주소로 들어오는 요청들을 home에서 처리하겠다는 뜻
 
 module.exports = app;
