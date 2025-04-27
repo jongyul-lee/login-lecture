1e6d0b9eeb0eec41f16b33c1567b3bdd8f250e67
jongyul-lee <dlwhddbf0614@gmail.com>
2025-04-27 22:32:52 +0900

    bodyParser 추가

--- app/app.js
+++ app/app.js
@@ -2,10 +2,15 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // 라우팅
 const home = require("./src/routes/home")
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.use("/", home); // use -> middle ware를 등록해주는 메서드. / 주소로 들어오는 요청들을 home에서 처리하겠다는 뜻
 
 app.set("views", "./src/views");
